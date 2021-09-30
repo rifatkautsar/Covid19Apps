4d6d87acef36db294243707ffab399e0076934fe
Rifat Kautsar <kautsarrifat@gmail.com>
2021-09-30 19:30:24 +0000

    Implements UI Statistik

--- app/src/main/java/com/rifat/covid19app/view/Fragment/FragmentStatistik.java
+++ app/src/main/java/com/rifat/covid19app/view/Fragment/FragmentStatistik.java
@@ -1,5 +1,6 @@
 package com.rifat.covid19app.view.Fragment;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,15 +9,81 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.rifat.covid19app.R;
+
+import java.util.ArrayList;
 
 
 public class FragmentStatistik extends Fragment {
+
+    LineChart lineChart;
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_statistik, container, false);
+
+        View view =inflater.inflate(R.layout.fragment_statistik, container, false);
+
+
+        lineChart = (LineChart) view.findViewById(R.id.lineChart);
+        LineDataSet lineDataSet = new LineDataSet(lineChartDataSet(),"data set");
+        ArrayList<ILineDataSet> iLineDataSets = new ArrayList<>();
+        iLineDataSets.add(lineDataSet);
+
+        LineData lineData = new LineData(iLineDataSets);
+        lineChart.setData(lineData);
+        lineChart.invalidate();
+
+
+        //if you want set background color use below method
+        //lineChart.setBackgroundColor(Color.RED);
+
+        // set text if data are are not available
+        lineChart.setNoDataText("Data not Available");
+
+        //you can modify your line chart graph according to your requirement there are lots of method available in this library
+
+        //now customize line chart
+
+        lineDataSet.setColor(Color.BLUE);
+        lineDataSet.setCircleColor(Color.GREEN);
+        lineDataSet.setDrawCircles(true);
+        lineDataSet.setDrawCircleHole(true);
+        lineDataSet.setLineWidth(5);
+        lineDataSet.setCircleRadius(10);
+        lineDataSet.setCircleHoleRadius(10);
+        lineDataSet.setValueTextSize(10);
+        lineDataSet.setValueTextColor(Color.BLACK);
+
+
+
+        return view;
+    }
+
+
+
+    private ArrayList<Entry> lineChartDataSet(){
+        ArrayList<Entry> dataSet = new ArrayList<Entry>();
+
+        dataSet.add(new Entry(0,40));
+        dataSet.add(new Entry(1,10));
+        dataSet.add(new Entry(2,15));
+        dataSet.add(new Entry(3,12));
+        dataSet.add(new Entry(4,20));
+        dataSet.add(new Entry(5,50));
+        dataSet.add(new Entry(6,23));
+        dataSet.add(new Entry(7,34));
+        dataSet.add(new Entry(8,12));
+        return  dataSet;
+
+
     }
 }
