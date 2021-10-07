61b5d5d843608fa796c34b5c323f1f6f1138138d
Rifat Kautsar <kautsarrifat@gmail.com>
2021-10-07 09:57:56 +0000

    Adding Activity DetailProvinsi

---
+++ app/src/main/java/com/rifat/covid19app/view/Activity/DetailProvinsiActivity.java
@@ -0,0 +1,20 @@
+package com.rifat.covid19app.view.Activity;
+
+import android.os.Bundle;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.rifat.covid19app.R;
+
+/**
+ * Created by Rifat on 10/5/21.
+ */
+public class DetailProvinsiActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailprovinsi);
+
+    }
+}
